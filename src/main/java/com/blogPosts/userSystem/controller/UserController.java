549bf1c754e1c9dee09e150489511896a89f7a7a
chloe <chloejs.horta@gmail.com>
2022-12-19 11:03:26 +0000

    small edit

--- src/main/java/com/blogPosts/userSystem/controller/UserController.java
+++ src/main/java/com/blogPosts/userSystem/controller/UserController.java
@@ -20,7 +20,6 @@ public class UserController {
         userService.saveUser(user);
         return "New user review is added"; // message passed to postman with 200 pass
     }
-    ////add this comment
 
     @GetMapping("/getAll")
     public List<User> getAllUsers(){
